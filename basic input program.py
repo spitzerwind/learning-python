7657079d55e410fb87766e190433c96261867847
spitzerwind <devyash988@gmail.com>
2026-06-07 01:13:54 +0530

    Add files via upload

---
+++ basic input program.py
@@ -0,0 +1,10 @@
+""" day 1"""
+
+
+
+print ("welcome to the band name Generator")
+
+c1 = input("what city did you grow up in?\n")
+c2 = input("what is your pet name?\n")
+
+print(" band name:" +c1 + " "+c2)
