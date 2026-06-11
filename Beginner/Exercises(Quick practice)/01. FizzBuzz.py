f95c4f202948948c465d8d810e3109628fd93f74
spitzerwind <spitzerwind@gmail.com>
2026-06-11 17:25:21 +0530

    Create 01. FizzBuzz.py

---
+++ Beginner/Exercises(Quick practice)/01. FizzBuzz.py
@@ -0,0 +1,14 @@
+for item in range (1,101):
+    
+    if item%15==0:
+        print("FizzBuzz")
+    
+    elif item%3==0:
+        print("Fizz")
+        
+    elif item%5==0:
+        print("Buzz")
+        
+        
+    else:
+        print(item)
